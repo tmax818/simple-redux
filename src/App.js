8a5de865deea263c7b4da612f96e4e2f57fb5d40
tmax818 <tylermaxwell661@gmail.com>
2020-07-17 21:28:26 -0700

    finish

--- src/App.js
+++ src/App.js
@@ -3,12 +3,31 @@ import "./App.css";
 
 import Counter from "./Counter";
 
-function App() {
-  return (
-    <div className="App">
-      <Counter />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  onCounterInc = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  onCounterDec = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1,
+    }));
+  };
+  render() {
+    return (
+      <div className="App">
+        <Counter
+          count={this.state.count}
+          inc={this.onCounterInc}
+          dec={this.onCounterDec}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -1,30 +1,14 @@
 import React, { Component } from "react";
 
-class Counter extends Component {
-  state = {
-    count: 0,
-  };
-
-  onCounterInc = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  onCounterDec = () => {
-    this.setState((prevState) => ({
-      count: prevState.count - 1,
-    }));
-  };
-
-  render() {
-    return (
-      <div>
-        Count: {this.state.count}
-        <br />
-        <button onClick={this.onCounterInc}>+</button>
-        <button onClick={this.onCounterDec}>-</button>
-      </div>
-    );
-  }
-}
+const Counter = (props) => {
+  return (
+    <div>
+      Count: {props.count}
+      <br />
+      <button onClick={props.inc}>+</button>
+      <button onClick={props.dec}>-</button>
+    </div>
+  );
+};
 
 export default Counter;
