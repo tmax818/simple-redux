a97507c231d7492760598a07146117936873a3f7
tmax818 <tylermaxwell661@gmail.com>
2020-07-22 15:16:28 -0700

    remove console.log

--- src/Actions.js
+++ src/Actions.js
@@ -1,6 +1,4 @@
 import * as ActionTypes from "./ActionTypes";
 
-console.log(typeof ActionTypes, ActionTypes);
-
 export const inc = () => ({ type: ActionTypes.INC });
 export const dec = () => ({ type: "DEC" });
